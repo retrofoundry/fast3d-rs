ecce2521c20ddc2b67db87fa42f07ae60c46fb8b
David Chavez <david@dcvz.io>
2023-07-14 12:34:42 +0200

    fmt

--- fast3d/src/gbi.rs
+++ fast3d/src/gbi.rs
@@ -32,9 +32,10 @@ pub struct GBICommandRegistry {
 }
 
 impl GBICommandRegistry {
-
     pub fn new() -> GBICommandRegistry {
-        GBICommandRegistry { gbi_opcode_table: HashMap::with_hasher(BuildNoHashHasher::default()) }
+        GBICommandRegistry {
+            gbi_opcode_table: HashMap::with_hasher(BuildNoHashHasher::default()),
+        }
     }
 
     pub fn setup(&mut self, rsp: &mut RSP) {
