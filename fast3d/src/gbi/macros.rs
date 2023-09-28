615fd02550b206e13ed3e08b673cbf04a7ef5274
dcvz <david@dcvz.io>
2023-09-28 15:02:55 +0200

    Make clippy happy

--- fast3d/src/gbi/macros.rs
+++ fast3d/src/gbi/macros.rs
@@ -1,8 +1,21 @@
+/// Define a GBI command macro.
+///
+/// This macro simplifies the creation of GBI command functions.
+/// It takes a name and a processing expression and generates a function with the specified name.
+///
+/// # Example
+///
+/// ```rust ignore
+/// gbi_command!(my_command, |params| {
+///     // processing logic here
+/// });
+/// ```
 macro_rules! gbi_command {
     ($name:ident, $process:expr) => {
         #[allow(non_snake_case)]
         pub(crate) fn $name(params: &mut GBICommandParams) -> GBIResult {
-            $process(params)
+            let process_closure = $process;
+            process_closure(params)
         }
     };
 }
